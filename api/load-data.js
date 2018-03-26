acf657492272ab960d39bd9423a71b4a7c8918ea
migrow <michael.growette@gmail.com>
2018-03-26 17:24:11 -0400

    app home route

---
+++ api/load-data.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const PouchDB = require('pouchdb-core')
+PouchDB.plugin(require('pouchdb-adapter-http'))
+const db = new PouchDB(`${process.env.COUCH_URL}${process.env.COUCH_DBNAME}`)
+
+db.bulkDocs([{}])
--- app/src/pages/home/index.js
+++ app/src/pages/home/index.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Home = props => {
   return (
     <div>
-      <h1> Home page </h1>
-      <img src="/jrs-innovation.png" alt="JRS innovation logo" />
+      <center>
+        <h1> Home page </h1>
+        <img src="/jrs-innovation.png" alt="JRS innovation logo" />
+      </center>
     </div>
   )
 }
