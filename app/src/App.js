24709ae4daf9fd1e864198a7a46c258a134dfd7f
migrow <michael.growette@gmail.com>
2018-03-26 12:36:16 -0400

    added app route to home page

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/home'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App
---
+++ app/src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = props => {
+  return (
+    <div>
+      <h1> Home page </h1>
+      <img src="/jrs-innovation.png" alt="JRS innovation logo" />
+    </div>
+  )
+}
+
+export default Home
