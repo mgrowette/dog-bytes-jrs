e88e340ed07e3d7a5652c680c903f351452796f0
migrow <michael.growette@gmail.com>
2018-03-29 10:48:49 -0400

    add video form

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './pages/home'
 import Videos from './pages/videos'
 import Video from './pages/videos/show'
+import AddVideo from './pages/videos/addVideo'
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/videos" component={Videos} />
+            <Route exact path="/videos/add" component={AddVideo} />
             <Route path="/videos/:id" component={Video} />
           </Switch>
         </div>
--- app/src/pages/videos/addVideo.js
+++ app/src/pages/videos/addVideo.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { FormControl } from 'material-ui/Form'
 import TextField from 'material-ui/TextField'
-import { addVideo } from '../../action-creators/videos'
 import { connect } from 'react-redux'
+import MenuAppBar from '../../components/MenuAppBar'
+import Button from 'material-ui/Button'
+import { changeVideo, addVideo, cancel } from '../../action-creators/videos'
+import { split } from 'ramda'
 
-const addVideo = props => {
+const AddVideo = props => {
   return (
     <div>
       <MenuAppBar title="Add a Video" {...props} showBackArrow={true} />
@@ -17,7 +20,44 @@ const addVideo = props => {
             value={props.video.name}
             onChange={e => props.onChange('name', e.target.value)}
           />
+          <TextField
+            id="instructor"
+            label="Video Instructor"
+            margin="normal"
+            value={props.video.instructor}
+            onChange={e => props.onChange('instructor', e.target.value)}
+          />
+          <TextField
+            id="desc"
+            label="Video Description"
+            margin="normal"
+            value={props.video.desc}
+            onChange={e => props.onChange('desc', e.target.value)}
+          />
+          <TextField
+            id="url"
+            label="Youtube Video URL"
+            margin="normal"
+            value={props.video.youTubeVideoURL}
+            onChange={e => props.onChange('url', e.target.value)}
+          />
+          <TextField
+            id="tags"
+            label="Video Content Tags"
+            margin="normal"
+            value={props.video.tags}
+            onChange={e => props.onChange('tags', e.target.value)}
+          />
         </FormControl>
+        <Button
+          variant="flat"
+          component="span"
+          color="primary"
+          onClick={props.onSubmit(props.history, props.video)}
+        >
+          Submit
+        </Button>
+        <Button onClick={props.cancel(props.history)}>Cancel</Button>
       </form>
     </div>
   )
@@ -31,10 +71,19 @@ const mapStateToProps = state => {
 
 const mapActionsToProps = dispatch => {
   return {
-    onChange: (field, value) => dispatch(changeVideo(field, value))
+    onChange: (field, value) => dispatch(changeVideo(field, value)),
+    onSubmit: (history, video) => e => {
+      e.preventDefault()
+      video.tags = split(' ', video.tags)
+      console.log('VIDEO TAGS', video.tags)
+      dispatch(addVideo(video, history))
+    },
+    cancel: history => e => {
+      dispatch(cancel(history))
+    }
   }
 }
 
 const connector = connect(mapStateToProps, mapActionsToProps)
 
-export default connector(addVideo)
+export default connector(AddVideo)
